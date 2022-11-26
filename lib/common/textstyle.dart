54b46ca15d8a947cd4514a862d30bc1031701fe4
Ipannn <ivakaopsionalacount@gmail.com>
2022-11-26 13:41:35 +0700

    2

---
+++ lib/common/textstyle.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+TextStyle Tittle = TextStyle(
+    fontSize: 36,
+    color: Colors.white,
+    fontFamily: 'Poppins',
+    fontWeight: FontWeight.bold
+
+);
+TextStyle subHeader = TextStyle(
+    fontSize: 26,
+    color: Colors.grey,
+    fontFamily: 'Poppins'
+);
---
+++ lib/splashscreen/splashscreen.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:pasanglagi/loginpage/login_user.dart';
+
+class Splashscreen extends StatefulWidget {
+
+  _Splashscreen createState() => _Splashscreen();
+}
+
+class _Splashscreen extends State<Splashscreen> {
+  void initState() {
+    super.initState();
+    splashscreenStart();
+  }
+
+  splashscreenStart() async {
+    var duration = const Duration(seconds: 3);
+    return Timer(duration, () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginUser()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: Image(image: AssetImage('assets/logo/pasanglagi.png'),),
+          )
+        ),
+      );
+  }
+}
