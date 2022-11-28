8b5cbf528731d25a6176da3987ecaf86a63b5266
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-11-28 07:31:45 +0700

    added a fucking splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
+
+  // Oh my, don't they hate to see us try?
+  // Come on, say what you think, it won't mean a thing.
 }
 
 class MyApp extends StatelessWidget {
@@ -9,6 +15,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    FlutterNativeSplash.remove();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
